3f1c7b5269b7da8dd38fa0207480739a4e494343
Kabi krishna <124856639+kabi-60@users.noreply.github.com>
2024-04-22 16:25:27 +0530

    Add files via upload

---
+++ day6q2/src/test/java/com/example/day6q2/Day6q2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day6q2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day6q2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
